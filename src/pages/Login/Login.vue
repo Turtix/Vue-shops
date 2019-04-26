cb6e1f46964b2d27632d54b8d20c06622b904690
Xia Chen <18339953861@163.com>
2019-04-26 14:28:41 +0800

    登录页面纯前台交效果 及简单校验

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -4,40 +4,46 @@
       <div class="login_header">
         <h2 class="login_logo">硅谷外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;" >密码登录</a>
+          <a href="javascript:;" :class="{on: isShowSms}" @click="isShowSms=true">短信登录</a>
+          <a href="javascript:;" :class="{on: !isShowSms}" @click="isShowSms=false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <div :class="{on: isShowSms}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification" >获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button :disabled="!isRightPhone" class="get_verification" :class="{right_phone_number: isRightPhone}" @click.prevent="sendCode">
+                {{computeTime>0 ? '已发送('+computeTime+')s': '获取验证码'}}
+              </button>
             </section>
             <section class="login_verification">
-              <input type="tel" maxlength="8" placeholder="验证码">
+              <input type="tel" maxlength="8" placeholder="验证码" v-model="code">
             </section>
             <section class="login_hint">
               温馨提示：未注册硅谷外卖帐号的手机号，登录时将自动注册，且代表已同意
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <div :class="{on: !isShowSms}">
             <section>
               <section class="login_message">
-                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
+                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名" v-model="name">
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <input :type="isShowPwd ? 'text': 'password' " maxlength="8" placeholder="密码" v-model="pwd">
+                <div class="switch_button " :class="isShowPwd ? 'on': 'off'"  @click="isShowPwd = !isShowPwd">
+                  <div class="switch_circle" :class="{right: isShowPwd}"></div>
+                  <span class="switch_text">{{isShowPwd ? 'abc': ''}}</span>
                 </div>
+              </section>
+              <section class="login_message">
+                <input type="text" maxlength="11" placeholder="验证码" v-model="captcha">
+                <img class="get_verification" src="./images/captcha.svg" alt="captcha">
               </section>
             </section>
           </div>
-          <button class="login_submit">登录</button>
+          <button class="login_submit" @click.prevent="login">登录</button>
         </form>
         <a href="javascript:;" class="about_us">关于我们</a>
       </div>
@@ -52,7 +58,60 @@
   export default {
     name: 'Login',
     data () {
-      return {}
+      return {
+        isShowSms: true, // 短信登录  false: 密码登录
+        phone: '', // 手机号
+        code: '',  //一次性短信验证码
+        name: '', // 用户名
+        pwd: '',  // 密码
+        captcha: '', // 一次性图片验证码
+        computeTime: 0, // 倒计时剩余的时间
+        isShowPwd: false, // 是否原样显示密码
+      }
+    },
+    computed: {
+      isRightPhone(){
+        return  /^1\d{10}$/.test(this.phone)
+      }
+    },
+    methods: {
+      sendCode() {
+        // 倒计时发送验证码剩余的时间
+        this.computeTime = 10
+        const intervalId = setInterval(()=>{
+          this.computeTime--
+          if (this.computeTime <= 0) {
+            clearInterval(intervalId)
+          }
+        },1000)
+      },
+      login() {
+        // 进行前端检验
+        const  { isShowSms,phone,isRightPhone,code,name,pwd,captcha } = this
+        // 如果是短信登录
+        if(isShowSms) {
+          if (!isRightPhone) {
+            return alert('请输入正确的手机号!')
+          }else if(! /^\d{6}$/.test(code)){
+            // 验证码必须是6位数字
+            return alert('请输入正确的短信验证码!')
+          }
+        }else {
+          // 如果是密码登录
+          if(!name.trim()){
+            // 用户名不能为空
+            return alert('请输入用户名!')
+          }else if(!pwd.trim()) {
+            // 用户名不能为空
+            return alert('请输入密码!')
+          }else if(!/^.{4}/.test(captcha)){
+            return alert('请输入正确的验证码!')
+          }
+        }
+
+        // 全部通过发送登录请求
+        console.log('检验通过 发送登录请求!')
+      },
     }
   }
 </script>
@@ -118,6 +177,8 @@
                 color #ccc
                 font-size 14px
                 background transparent
+                &.right_phone_number
+                  color black
             .login_verification
               position relative
               margin-top 16px
@@ -156,7 +217,9 @@
                   border-radius 50%
                   background #fff
                   box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
-                  transition transform .3s
+                  transition transform .3
+                  &.right
+                    transform translateX(27px)
             .login_hint
               margin-top 12px
               color #999
